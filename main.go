fe5f5519f384ba208d14ab93129e9994eed54596
Bhushan Gupta <bhushan.gupta@gslab.com>
2021-07-05 19:27:43 +0530

    update article role api added

--- main.go
+++ main.go
@@ -30,7 +30,29 @@ import (
 var SECRET_KEY = []byte("gosecretkey")
 var Client *mongo.Client
 
-// var Key *ecdsa.PrivateKey
+func getHash(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(hash)
+}
+
+func GenerateJWT(userID int, companyID int, userRole string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["authorized"] = true
+	claims["userid"] = userID
+	claims["companyid"] = companyID
+	claims["userrole"] = userRole
+	claims["exp"] = time.Now().Add(time.Minute * 30)
+	tokenString, err := token.SignedString(SECRET_KEY)
+	if err != nil {
+		log.Println("Error in JWT token generation")
+		return "", err
+	}
+	return tokenString, nil
+}
 
 func main() {
 	authorization.SECRET_KEY = SECRET_KEY
@@ -54,14 +76,14 @@ func main() {
 
 	router.Handle("/{company}/article/{articleid}", authorization.IsAuthorized(authEnforcer, GetArticlesHandler)).Methods("GET")
 	router.Handle("/{company}/article/{articleid}", authorization.IsAuthorized(authEnforcer, DeleteArticleHandler)).Methods("DELETE")
-	router.Handle("/{company}/articlerole/{articleid}", authorization.IsAuthorized(authEnforcer, UpdateArticleRoleHandler)).Methods("PATCH")
-	router.Handle("/{company}/role", authorization.IsAuthorized(authEnforcer, UpdateCompanyRoleHandler)).Methods("PUT")
+	router.Handle("/articlerole/{articleid}", authorization.IsAuthorized(authEnforcer, UpdateArticleRoleHandler)).Methods("PUT")
+	//router.Handle("/role", authorization.IsAuthorized(authEnforcer, UpdateCompanyRoleHandler)).Methods("PUT")
 	// router.HandleFunc("/article/{company}", CreateArticleHandler).Methods("POST")
 	// router.HandleFunc("/article/{company}", DeleteArticleHandler).Methods("DELETE")
 	// router.Use(authorization.Authorizer(authEnforcer))
 
-	log.Print("Server started on localhost:8000")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Print("Server started on localhost:8040")
+	log.Fatal(http.ListenAndServe(":8040", router))
 }
 
 func GetArticlesHandler(response http.ResponseWriter, request *http.Request, claims jwt.MapClaims) {
@@ -190,30 +212,6 @@ func AddUser(response http.ResponseWriter, request *http.Request, claims jwt.Map
 	json.NewEncoder(response).Encode(result)
 }
 
-func getHash(pwd []byte) string {
-	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
-	if err != nil {
-		log.Println(err)
-	}
-	return string(hash)
-}
-
-func GenerateJWT(userID int, companyID int, userRole string) (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
-	claims := token.Claims.(jwt.MapClaims)
-	claims["authorized"] = true
-	claims["userid"] = userID
-	claims["companyid"] = companyID
-	claims["userrole"] = userRole
-	claims["exp"] = time.Now().Add(time.Minute * 30)
-	tokenString, err := token.SignedString(SECRET_KEY)
-	if err != nil {
-		log.Println("Error in JWT token generation")
-		return "", err
-	}
-	return tokenString, nil
-}
-
 func DeleteArticleHandler(w http.ResponseWriter, r *http.Request, claims jwt.MapClaims) {
 	vars := mux.Vars(r)
 	articleID := vars["articleid"]
@@ -260,4 +258,64 @@ func DeleteArticleHandler(w http.ResponseWriter, r *http.Request, claims jwt.Map
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"message: successfully  deleted article"}`))
+}
+
+func UpdateArticleRoleHandler(w http.ResponseWriter, r *http.Request, claims jwt.MapClaims) {
+	w.Header().Set("Content-Type", "application/json")
+	var articleRole model.ArticleRole
+
+	err := json.NewDecoder(r.Body).Decode(&articleRole)
+	if err != nil {
+		authorization.WriteError(http.StatusBadRequest, "DECODE ERROR", w, err)
+		return
+	}
+
+	companyId := int(claims["companyid"].(float64))
+
+	if articleRole.CompanyId != companyId {
+		authorization.WriteError(http.StatusBadRequest, "Please provide correct company id ", w, err)
+		return
+	}
+
+	if articleRole.Role != "admin" && articleRole.Role != "member" && articleRole.Role != "anonymous" {
+		authorization.WriteError(http.StatusBadRequest, "invalid role provided", w, errors.New("invalid role"))
+		return
+	}
+	superadminId := int(claims["userid"].(float64))
+
+	if superadminId == articleRole.UserId {
+		authorization.WriteError(http.StatusBadRequest, "Cannot change role on article for superadmin", w, err)
+		return
+	}
+
+	var role model.Roles
+
+	fmt.Println("#####################")
+	collection := Client.Database("SPEC-CENTER").Collection("role")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = collection.FindOne(ctx, primitive.M{"userid": articleRole.UserId, "companyid": articleRole.CompanyId}).Decode(&role)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"message":"` + "Please provide correct Details. " + err.Error() + `"}`))
+		return
+	}
+	fmt.Println("!!!!!!!!!!!!!!!!!!!!!",role)
+	if role.Role == "anonymous"{
+		authorization.WriteError(http.StatusBadRequest, "BAD REQUEST:- anonymous user can't be given article access", w, errors.New("BAD REQUEST"))
+		return
+	}
+
+	fmt.Println("%%%%%%%%%%%%%%")
+	collection1 := Client.Database("SPEC-CENTER").Collection("articlerole")
+	opts := options.Update().SetUpsert(true)
+	filter := primitive.M{"userid": articleRole.UserId, "companyid": articleRole.CompanyId, "articleid": articleRole.ArticleId}
+	update := bson.D{{"$set", bson.D{{"role", articleRole.Role}}}}
+
+	_, err = collection1.UpdateOne(ctx, filter, update, opts)
+	if err != nil {
+		authorization.WriteError(http.StatusInternalServerError, "update error", w, errors.New("update error"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message":"` + fmt.Sprintf("Role for userid:%d for articleid: %d is changed to: %s",articleRole.UserId,articleRole.ArticleId,articleRole.Role) + `"}`))
 }
